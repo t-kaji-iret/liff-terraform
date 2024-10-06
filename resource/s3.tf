d7c69b8069dd61758598e69fb624c7fd5d778815
miura-toya <t-miura@icloudpack.jp>
2024-10-06 15:27:21 +0900

    add: 既存リソースをAWS Moduleを使用して定義

--- resource/s3.tf
+++ resource/s3.tf
@@ -1,43 +1,20 @@
-# プライベートバケット
-
-# バックエンドのソースを保存する
+#########################
+# S3
+#########################
 # NOTE: バケットポリシーではなくLambdaやIAMユーザーなどアクセスする側に権限付与する設計
-resource "aws_s3_bucket" "private-backend" {
-  bucket = "gourmet-liff-app-backend-source-bucket"
-}
-resource "aws_s3_bucket_public_access_block" "private-backend" {
-  bucket = aws_s3_bucket.private-backend.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+module "backend-source-private-bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "gourmet-liff-app-backend-source-public-bucket"
 }
 
-# NOTE: OAIを設定して、バケットポリシーでOAIからのアクセスのみを許可するように制御する設計
+# NOTE: OACを設定して、バケットポリシーでOACからのアクセスのみを許可するように制御する設計
 # TODO: CloudFront、OAC、OACからのアクセスを許可するバケットポリシーするの作成が必要(OACはバケットポリシーじゃないと制御できない)
-# フロントエンドのソースを保存するバケット
-resource "aws_s3_bucket" "private-frontend" {
-  bucket = "gourmet-liff-app-frontend-source-bucket"
-}
-resource "aws_s3_bucket_public_access_block" "private-frontend" {
-  bucket = aws_s3_bucket.private-frontend.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+module "frontend-source-private-bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "gourmet-liff-app-frontend-source-private-bucket"
 }
 
-# 画像を保存するバケット
-resource "aws_s3_bucket" "private-photo" {
-  bucket = "gourmet-liff-app-photo-bucket"
-}
-resource "aws_s3_bucket_public_access_block" "private-photo" {
-  bucket = aws_s3_bucket.private-photo.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+module "photo-private-bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "gourmet-liff-app-photo-private-bucket"
 }
---
+++ resource/vpc.tf
@@ -0,0 +1,40 @@
+#########################
+# VPC
+#########################
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "gourmet-liff-app-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Project   = "gourmet-liff-app"
+  }
+}
+
+#########################
+# VPC EndPoint
+#########################
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  tags = {
+    Terraform = "true"
+    Project   = "gourmet-liff-app"
+    Name     = "gourmet-liff-app-s3-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = module.vpc.private_route_table_ids[0]
+}
